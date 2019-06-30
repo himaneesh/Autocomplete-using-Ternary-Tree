29e1adfe87a345f4732863d55345fe93511830a7
himaneesh <himaneeshpathak@gmail.com>
2019-06-30 17:22:51 -0400

    Inserting in a Ternary Tree

---
+++ src/com/tt/impl/TreeNode.java
@@ -0,0 +1,13 @@
+package com.tt.impl;
+
+public class TreeNode {
+	char val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode middle;
+	boolean isEnd;
+
+	public TreeNode(char val) {
+		this.val = val;
+	}
+}
